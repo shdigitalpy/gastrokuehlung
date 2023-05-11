5c04574b06b6bd4c0389fca3f2ae7e965da3547a
Sandro Huber <sandro@sh-digital.ch>
2023-05-11 10:40:42 +0200

    gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import TagManager from 'react-gtm-module'
-const tagManagerArgs = {
-    gtmId: 'GTM-WZTGGWF'
-}
-if (process.browser) {
-  TagManager.initialize(tagManagerArgs);
-}
+import Script from 'next/script'
+
 export default function Document() {
   return (
     <Html lang="de">
-      <Head />
+      <Head>
+       <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WZTGGWF');`}}></Script>
+      </Head>
       <body>
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZTGGWF"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         <Main />
         <NextScript />
       </body>
